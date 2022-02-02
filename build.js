e46529caeb2d69c293c3aeb7cd69ca62e960453e
Christian Bernier <christianbernier02@gmail.com>
2022-02-02 01:33:24 -0500

    latex, topic pages

--- build.js
+++ build.js
@@ -1,8 +1,14 @@
 // build.js
 // Creates the HTML pages in public/ for all the blog pages in notes/
 
+// TODO:
+// - index page
+// - add better navigation between pages
+
 const fs = require('fs-extra')
 const markdown = require("markdown-wasm")
+const katex = require('katex')
+const tc = require("title-case")
 const recursive = require("recursive-readdir")
 const matter = require('gray-matter')
 
@@ -14,27 +20,92 @@ const prettyDate = d => d.toLocaleString("en-US", { weekday: 'long', year: 'nume
 // ex. "notes/test/test-page.md" becomes "/public/test/test-page.html"
 const notePageUrl = mdFilepath => `/public/${mdFilepath.substring(6, mdFilepath.length - 3)}.html`
 
-// Goes through the .md files in notes/ and creates HTML versions of each note
-const generateNotePages = () => recursive("notes/", function (err, files) {
+// Returns the filepath of a topic page, given the topic
+// ex. "test-topic" becomes "/public/test-topic.html"
+const topicPageUrl = topic => `/public/${topic}.html`
+
+// Generates the HTML link tag for a note page, from the corresponding topic page
+const noteLinkFromTopicPage = (note, topic) => `<a class="note-link" href="${__dirname}/public/${topic}/${note}.html">${tc.titleCase(note.replace(/-/g, " "))}</a>`
+
+// Generates a list of topics found in the notes/ folder, and the notes associated with each topic
+const generateTopicStructure = files => {
+    let topics = []
+
+    for (const file of files) {
+        // splits filepath like "notes/relativity/conservation-laws.md"
+        // into fileTopic = "relativity"
+        // and  fileTitle = "conservation-laws"
+        const fileStructure = file.split("/")
+        const fileTopic = fileStructure[1]
+        const fileTitle = fileStructure[2].split(".")[0]
+
+        let found = false
+
+        // searches topics so far for the current file's topic
+        for (let topic of topics) {
+            if (topic.topic === fileTopic) {
+                topic.notes.push(fileTitle)
+                found = true
+                break
+            }
+        }
+
+        // if the topic is new, a new topic object is added
+        if (!found) {
+            topics.push({
+                topic: fileTopic,
+                notes: [
+                    fileTitle
+                ]
+            })
+        }
+    }
+
+    return topics
+}
+
+// Goes through the .md files in notes/ and creates HTML versions of each note and topic pages for each directory
+const generatePages = () => recursive("notes/", (err, files) => {
     if (err) {
         console.error(err)
         return
     }
 
+    // uses the list of all note files to generate the topic structure
+    const topicStructure = generateTopicStructure(files)
+
+    // generates a topic page for each topic found with notes in the notes/ folder
+    for (const topic of topicStructure) {
+        const template = fs.readFileSync("src/topic-template.html", "utf8")
+        const html = template
+            .replace(/TOPIC/g, tc.titleCase(topic.topic.replace(/-/g, " ")))
+            .replace(/NOTES/g, topic.notes.map(n => noteLinkFromTopicPage(n, topic.topic)).join(""))
+
+        console.log(`Generating topic page for ${topic.topic}`)
+        fs.outputFileSync(__dirname + topicPageUrl(topic.topic), html)
+    }
+
+    // generates a note page for each note found within the notes/ page
     for (const file of files) {
         if (file.substring(file.length - 3) !== ".md") continue
 
+        const topic = file.split("/")[1]
         const template = fs.readFileSync("src/note-template.html", "utf8")
-        const noteContents = matter(fs.readFileSync(file))
-        const markdownAsHtml = markdown.parse(noteContents.content)
+        const noteContents = matter(fs.readFileSync(file, "utf8"))
+        const markdownAsHtml = markdown.parse(noteContents.content.replace(/\$\$/g, "$$$$$$"))
         const date = prettyDate(noteContents.data.date)
-        const html = template.replace(/CONTENT/g, markdownAsHtml)
+        const html = template
+            .replace(/CONTENT/g, markdownAsHtml)
             .replace(/TITLE/g, noteContents.data.title)
+            .replace(/TOPIC_NAME/g, `← ${tc.titleCase(topic.replace(/-/g, " "))}`)
+            .replace(/TOPIC_LINK/g, `${__dirname}/public/${topic}.html`)
             .replace(/DATE/g, date)
+            .replace(/\$\$(.+?)\$\$/g, (_, latex) => katex.renderToString(latex.replace(/<\/?em>/g, "*"), { throwOnError: false, displayMode: true }))
+            .replace(/\$(.+?)\$/g, (_, latex) => katex.renderToString(latex.replace(/<\/?em>/g, "*"), { throwOnError: false }))
 
+        console.log(`Generating note page for ${file}`)
         fs.outputFileSync(__dirname + notePageUrl(file), html)
     }
-
 })
 
-generateNotePages()
+generatePages()
---
+++ notes/quantization-of-light-and-energy/thermal-radiation.md
@@ -0,0 +1,111 @@
+---
+title: Thermal Radiation
+date: 2022-02-01
+---
+
+## What is Thermal Radiation?
+
+Thermal radiation is the electromagnetic radiation emitted by all objects becuase of their temperature.
+
+Thermal radiation is usually in the infrared region, but sometimes objects are so hot that their radiation is visible light.
+
+### Thermal radiation experiments
+
+Experiments were conducted to try to measure how the intensity of the radiation changed as a function of wavelength.
+
+The experiments uncovered two characteristics:
+
+1. The total intensity radiated increases as the temperature increases (hotter objects glow brighter)
+
+2. The maximum wavelength, $\lambda_{max}$ decreases as the temperature increases (the wavelength of light is inversely proportional to the temperature)
+
+$$I=\sigma T^4$$
+
+where $\sigma$ is the *Stefan-Boltzmann constant*, about $5.67037*10^{-8} W/m^2*K^4$.
+
+### Wein's displacement law
+
+The maximum wavelength of radiation is inversely proportional to the temperature of the object.
+
+$$\lambda_{max}T=2.8978*10^{-3} m*K$$
+
+Hot objects are first red, then white, then blue.
+
+### Blackbody radiation
+
+A **blackbody** is an object which absorbs all radiation incident on it and reflects none of the incident radiation.
+
+Physicists consider a cavity filled with electromagnetic radiation in thermal equilibrium with its walls at temperature $T$ as the ideal blackbody.
+
+The radiation inside the box has an energy density of $u(\lambda)$, as a function of its wavelength.
+
+At any given time, half of the radiation moves away from the hole and half moves towards it, at speed $c$. However, because the radiation travels at all sorts of angles, the energy flowing perpendicular to the surface of the hole is $1/2$ of the total.
+
+$$I(\lambda)=\frac{c}{4}u(\lambda)$$
+
+To find the total intensity between two wavelengths $\lambda_1$ and $\lambda_2$, we use:
+
+$$I(\lambda_1 : \lambda_2) = \int_{\lambda_1}^{\lambda_2} I(\lambda) d\lambda$$
+
+The total emitted intensity is therefore
+
+$$I=\int_0^{\infty} I(\lambda) d\lambda$$
+
+## Classical Theory of Thermal Radiation
+
+Classical assumptions yield the following theories:
+
+1. Blackbody boxes are filled with standing waves (since there is no electric field inside a metal box)
+2. For a box of volume $V$, the following equation gives the number of standing waves:
+
+$$N(\lambda)d\lambda=\frac{8\pi V}{\lambda^4} d\lambda$$
+
+3. Each wave contributes an average energy of $kT$ to the radiation in the box.
+
+Therefore, classical physics calculates the total energy density of radiation as follows:
+
+$$u(\lambda) d\lambda = \frac{N(\lambda)d\lambda}{V} kT=\frac{8\pi}{\lambda^4}kTd\lambda$$
+
+And the intensity per unit wavelength (Rayleigh-Jeans formula):
+
+$$I(\lambda)=\frac{c}{4}u(\lambda)=\frac{c}{4}\frac{8\pi}{\lambda^4}kT=\frac{2\pi c}{\lambda^4}kT$$
+
+This equation results in the prediction that wavelengths decrease to $0$ as the energy increases, which is not true. This is called the *ultraviolet catastrophe*.
+
+## Quantum Theory of Thermal Radiation
+
+Max Planck tried to fix the ultraviolet catasrophe, and used a new physical theory called **quantum physics**, led by **wave mechanics** also known as **quantum mechanics**.
+
+Quantum physics is based on the idea that an oscillating atom can absorb and emit energy only as discrete bundles.
+
+In Planck's theory, the energy can only be integer multiples of a certain base quantity of energy:
+
+$$E_n=n\epsilon, n=1, 2, 3...$$
+
+Where
+
+$$\epsilon=hf$$
+
+$h$ is known as **Planck's constant**.
+
+Planck's ideas predict the number of oscillators with energy $E_n$ is
+
+$$N_n=N(1-e^{-\epsilon/kT})e^{-n\epsilon/kT}$$
+
+Therefore,
+
+$$E_{av}=\frac{1}{N}\sum_{n=0}^\infty N_n E_n=(1-e^{-\epsilon/kT})\sum_{n=0}^\infty (n\epsilon)e^{-n\epsilon/kT}$$
+
+So
+
+$$E_{av}=\frac{\epsilon}{e^{\epsilon/kT} - 1}=\frac{hf}{e^{hf/kT} - 1}=\frac{hc/\lambda}{e^{hc/\lambda kT} - 1}$$
+
+The intensity of the radiation becomes
+
+$$I(\lambda) = \frac{c}{4}\left(\frac{8\pi}{\lambda^4}\right)\left[\frac{hc/\lambda}{e^{hc/\lambda kT} - 1}\right]=\frac{2\pi hc^2}{\lambda^5}\frac{1}{e^{hc/\lambda kT} - 1}$$
+
+The relationship between the stefan-Boltzmann constant and Planck's constant:
+
+$$\sigma=\frac{2\pi^5k^4}{15c^2h^3}$$
+
+Using the equations above, it is possible to determine the temperature of an object using only one wavelength of its thermal radiation. A *radiometer* measures the intensity of specific wavelengths of thermal radiation.
