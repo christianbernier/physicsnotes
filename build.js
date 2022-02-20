9e83018ea833b56638f3627488257e53a215de8e
Christian Bernier <christianbernier02@gmail.com>
2022-02-20 13:44:18 -0500

    share images

--- build.js
+++ build.js
@@ -1,14 +1,12 @@
 // build.js
 // creates the HTML pages in public/ for all the blog pages in notes/
 
-// TODO
-// - programmatically create shareimages
-
 const fs = require('fs-extra')
 const markdown = require("markdown-wasm")
 const katex = require('katex')
 const spellchecker = require('spellchecker')
 const matter = require('gray-matter')
+const canvas = require('canvas')
 
 // should the files be checked for spelling?
 const CHECK_SPELLING = false
@@ -30,7 +28,7 @@ const prettyDate = d => d.toLocaleString("en-US", { weekday: 'long', year: 'nume
 const notePageLinkTag = (note, topic) => `<a class="note-link" href="${((process.env.NETLIFY) ? "" : `${__dirname}/public/`) + topic.folder}/${note.file}.html">${note.name}</a>`
 
 // generates the HTMl link tag for a topic page, from the index page
-const topicPageLinkTag = topic => 
+const topicPageLinkTag = topic =>
     `<a href="${((process.env.NETLIFY) ? "" : `${__dirname}/public/`) + topic.folder}.html" class="topic-tile-link">    
         <div class="topic-tile-body">
             <p class="topic-tile-title">${topic.name}</p>
@@ -60,13 +58,15 @@ const generateNotePage = (note, topic) => {
     // reads the data in the header of each note (includes title and date)
     const noteData = matter(fs.readFileSync(`notes/${topic.folder}/${note.file}.md`, "utf8"))
 
+    generateImageForNote(note, topic)
+
     // converts note file (written in Markdown) into HTML
     let noteHtml = markdown.parse(
         noteData.content
-        .replace(/_/g, "\\_")
-        .replace(/\$\$/g, "$$$$$$")
-        .replace(/\.\.\/\.\.\/public/g, ".."))
-    
+            .replace(/_/g, "\\_")
+            .replace(/\$\$/g, "$$$$$$")
+            .replace(/\.\.\/\.\.\/public/g, ".."))
+
     // replace fields in the template with note data
     const noteDate = prettyDate(noteData.data.date)
     noteHtml = noteTemplate
@@ -74,6 +74,7 @@ const generateNotePage = (note, topic) => {
         .replace(/TITLE/g, note.name)
         .replace(/TOPIC_NAME/g, topic.name)
         .replace(/TOPIC_LINK/g, `../${topic.folder}.html`)
+        .replace(/SHARE_IMAGE/g, `../images/shareimages/${topic.folder}/${note.file}.png`)
         .replace(/DATE/g, noteDate)
         .replace(/\$\$(.+?)\$\$/g, (_, latex) => katex.renderToString(latex.replace(/<\/?em>/g, "*").replace(/<\/?del>/g, "~"), { throwOnError: false, displayMode: true, strict: ec => ec !== "newLineInDisplayMode" }))
         .replace(/\$(.+?)\$/g, (_, latex) => katex.renderToString(latex.replace(/<\/?em>/g, "*").replace(/<\/?del>/g, "~"), { throwOnError: false }))
@@ -92,7 +93,7 @@ const generateNotePage = (note, topic) => {
         for (const word of words) {
             // if the word is in the allowed words in the dictionary, skip it
             if (dictionary.allowedWords.indexOf(word.toLowerCase()) !== -1) continue
-            
+
             // if the word is misspelt, console.log it in red
             if (spellchecker.isMisspelled(word))
                 console.log('\u001b[' + 31 + 'm' + `POSSIBLE SPELLING ERROR: "${word}" found in ${topic.name}/${note.name}` + '\u001b[0m')
@@ -100,6 +101,71 @@ const generateNotePage = (note, topic) => {
     }
 
     fs.outputFileSync(__dirname + `/public/${topic.folder}/${note.file}.html`, noteHtml)
+}
+
+// given an array of lines of text, creates a string with lines
+// of max length maxChars with newline breaks
+// ex. given ["super long string"] with maxChars = 6: "super\nlong\nstring"
+const wrap = (lines, maxChars) => {
+    // if every line is within maxChars, return that as a string
+    if (lines.every(l => l.length <= maxChars)) return lines.join("\n")
+
+    // get the next line to be processed (at the end of lines)
+    let rest = lines.pop().split(" ")
+    let next = ""
+
+    // so long as the next line is within maxChars and there are
+    // more words to add, keep adding words to it
+    while (rest.length !== 0 && next.length + rest[0].length <= maxChars)
+        next += rest.shift() + " "
+
+    // get rid of the extra space at the end
+    next = next.substring(0, next.length - 1)
+
+    // keep doing this until the entire string is processed
+    return wrap([...lines, next, rest.join(" ")], maxChars)
+}
+
+const generateImageForNote = async (note, topic) => {
+    // set up image
+    const image = canvas.createCanvas(1200, 630)
+    const ctx = image.getContext("2d")
+
+    // background
+    ctx.fillStyle = "#EEEBE3"
+    ctx.fillRect(0, 0, 1200, 630)
+
+    // load topic image
+    const topicImg = await canvas.loadImage(topic.image.replace("transparent", "dark"))
+    ctx.drawImage(topicImg, 50, 50, 100, 100)
+
+    // load favicon
+    const faviconImg = await canvas.loadImage(`images/favicon.png`)
+    ctx.drawImage(faviconImg, 1050, 480, 100, 100)
+
+    // draws "physics.notes.cbernier.com" next to the favicon
+    ctx.fillStyle = "#020029"
+    ctx.font = "bold 48px PT Sans"
+    ctx.textAlign = "right"
+    ctx.textBaseline = "middle"
+    ctx.fillText("physics.notes.cbernier.com", 1030, 530)
+
+    // draws topic name next to the topic icon
+    ctx.textAlign = "left"
+    ctx.fillText(topic.name, 170, 100)
+
+    // draws note name
+    ctx.font = "900 72px PT Sans"
+    const wrappedText = wrap([note.name], 30)
+    const lines = wrappedText.split("\n").length
+    const yOffset = 36 * (lines - 1) // adjust for multiple lines of text
+    ctx.fillText(wrappedText, 50, 315 - yOffset)
+
+    // export image
+    const buffer = image.toBuffer()
+    const url = `${__dirname}/public/images/shareimages/${topic.folder}/${note.file}.png`
+    fs.createFileSync(url)
+    fs.writeFileSync(url, buffer)
 }
 
 generateIndexPage()
